3bf6ef33ad08005b50f52209068f57d5c2a2dce2
Gonzalo Allievi <gonza.10.cs@hotmail.com>
2021-12-14 23:29:01 -0300

    Desafio clase 3

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <nav className="nav">
+            <li><a href="#">RESTAURANT</a></li>
+            <li><a href="#">BARES</a></li>
+        </nav>
+    )
+}
+
+export default NavBar
+
+
+
+
+
